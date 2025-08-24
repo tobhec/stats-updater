fbb289536665e77932b7ade2ed7b593673b09d17
Tobias Hector <tobias.hector@hotmail.com>
2025-08-24 15:27:00 +0200

    Fixed the vintage saving to be per subscriber

--- revision_checker.R
+++ revision_checker.R
@@ -1,20 +1,22 @@
 # List all CSV files in directory
-vintage_names <- list.files("C:/Users/Tobia/raspberry_pi/stats-updater/vintages", full.names = TRUE)
+vintage_names <- list.files(paste0("C:/Users/Tobia/raspberry_pi/stats-updater/vintages/", per), full.names = TRUE)
 
-# Read them all into a list of data.tables
-vintages <- lapply(vintage_names, function(f) fread(f, header = TRUE))
-names(vintages) <- tools::file_path_sans_ext(basename(vintage_names))
-
-# Remove any newly added table from the vintage comparison
-shared_tables <- intersect(names(i_temp), names(vintages))
-indics_to_compare <- i_temp[shared_tables]
-vintages <- vintages[shared_tables]
-
-# Calculate differences between new data and vintages
-diff_list <- Map(calc_diff, indics_to_compare, vintages)
-
-# Make binary checks if data cells have been revised or not
-revision_list <- Map(check_for_revisions, diff_list)
+if(length(vintage_names) > 0){
+  # Read them all into a list of data.tables
+  vintages <- lapply(vintage_names, function(f) fread(f, header = TRUE))
+  names(vintages) <- tools::file_path_sans_ext(basename(vintage_names))
+  
+  # Remove any newly added table from the vintage comparison
+  shared_tables <- intersect(names(i_temp), names(vintages))
+  indics_to_compare <- i_temp[shared_tables]
+  vintages <- vintages[shared_tables]
+  
+  # Calculate differences between new data and vintages
+  diff_list <- Map(calc_diff, indics_to_compare, vintages)
+  
+  # Make binary checks if data cells have been revised or not
+  revision_list <- Map(check_for_revisions, diff_list)
+}
 
 
 
--- vintage_saver.R
+++ vintage_saver.R
@@ -1,11 +1,11 @@
 setwd("C:/Users/Tobia/raspberry_pi/stats-updater")
 
-#if (!dir.exists("./tessssst")) {
-#  dir.create("./tessssst", recursive = TRUE)
-#}
+if (!dir.exists(paste0("./vintages/", per))) {
+  dir.create(paste0("./vintages/", per), recursive = TRUE)
+}
 
 index <- 0
-for(i in indics) {
+for(i in i_temp) {
   index <- index + 1
-  fwrite(i, paste0("./vintages/", names(indics)[index]), sep = ";", col.names = TRUE)
+  fwrite(i, paste0(paste0("./vintages/", per, "/"), names(i_temp)[index]), sep = ";", col.names = TRUE)
 }
