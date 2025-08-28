ff797a9f74a74f0222330af3f07f6ea999d5b6e2
Tobias Hector <tobias.hector@hotmail.com>
2025-08-28 23:47:51 +0200

    Added exchange rates

--- data_filterer.R
+++ data_filterer.R
@@ -1,12 +1,22 @@
 # Filter out the unnecessary data from the data table
 if("obs_status" %in% names(i_temp[[code]])) {
-  i_temp[[code]][, obs_status := NULL]
+  i_temp[[code]] <- i_temp[[code]][, obs_status := NULL]
 }
-i_temp[[code]] <- i_temp[[code]][geo %in% countries & TIME >= as.numeric(period)]
+if("obs_conf" %in% names(i_temp[[code]])) {
+  i_temp[[code]] <- i_temp[[code]][, obs_conf := NULL]
+}
 
-# Change the country code to the real name
-i_temp[[code]] <- cc_dict[i_temp[[code]], on = "geo"]
-i_temp[[code]][, geo := NULL]
-setnames(i_temp[[code]], old = c("Country", "TIME", "obs_value"), new = c("Country", "Time", "Value"))
-
-i_temp[[code]] <- dcast(i_temp[[code]], Country ~ Time, value.var = "Value")
+if(con_geos_list[[code]])
+{
+  i_temp[[code]] <- i_temp[[code]][geo %in% countries & TIME >= as.numeric(period)]
+  
+  # Change the country code to the real name
+  i_temp[[code]] <- cc_dict[i_temp[[code]], on = "geo"]
+  i_temp[[code]][, geo := NULL]
+  setnames(i_temp[[code]], old = c("Country", "TIME", "obs_value"), new = c("Country", "Time", "Value"))
+  i_temp[[code]] <- dcast(i_temp[[code]], Country ~ Time, value.var = "Value")
+} else {
+  setnames(i_temp[[code]], old = c("TIME", "obs_value"), new = c("Time", "Value"))
+  i_temp[[code]] <- dcast(i_temp[[code]], rowid(Time) ~ Time, value.var = "Value")
+  i_temp[[code]] <- i_temp[[code]][, Time := NULL]
+}
