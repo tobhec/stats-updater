f7634fbe92e33f127339ac299c73e6ea81f95472
Tobias Hector <tobias.hector@hotmail.com>
2025-09-16 21:02:29 +0200

    Restructured the tryCatch logic to be more precise

--- main.R
+++ main.R
@@ -6,6 +6,7 @@ source("./config.R")
 
 for (per in names(pref_list)) {
   
+  # Try to define tables to be used in the script, based on the input by the subscriber
   tryCatch(
     {
       # Extract the selections by the subscriber
@@ -18,35 +19,64 @@ for (per in names(pref_list)) {
       units_temp <- lapply(units_list[names(units_list) %in% i_codes], copy)
       links_temp <- lapply(links_list[names(links_list) %in% i_codes], copy)
       dropdowns_temp <- lapply(dropdowns_list[names(dropdowns_list) %in% i_codes], copy)
-      
-      # Filter the data (i_temp) based on the selection made by the subscriber (per)
-      for (code in i_codes) {
-        
-        tryCatch(
-          {
-            cross_section_filters <- i_selection[[code]]$Filter
-            period   <- i_selection[[code]]$Period
-            source("./data_filterer.R")
-          },
-          error = function(e) {
-            cat("Error filtering data for: ", code, "\n", e$message)
-            return(NA)
-          }
-        )
-      }
-      
-      source("./revision_checker.R")
-      source("./mail_generator.R")
-      #source("./email_sender.R")
-      source("./vintage_saver.R")
-      
-      
     },
     error = function(e) {
-      cat("Error generating mail for: ", per, "\n", e$message)
+      cat("Error defining variables for: ", per, "\n", e$message)
       return(NA)
     }
   )
-
+      
+  # Try to filter the data (i_temp) based on the selection made by the subscriber (per)
+  tryCatch(
+    {
+      for (code in i_codes) {
+          cross_section_filters <- i_selection[[code]]$Filter
+          period   <- i_selection[[code]]$Period
+          source("./data_filterer.R")
+      }
+    },
+    error = function(e) {
+      cat("Error filtering data for: ", per, "\n", e$message)
+      return(NA)
+    }
+  )
+      
+  # Try to compare the data against the vintages to check for revisions
+  tryCatch(
+    {
+      source("./revision_checker.R")
+    },
+    error = function(e) {
+      cat("Error performing the revision check for:", per, "\n", e$message)
+      return(NA)
+    }
+  )
+  
+  # Try to generate the mail for the current subscriber
+  tryCatch(
+    {
+      # Generate the mail
+      source("./mail_generator.R")
+    },
+    error = function(e) {
+      cat("Error generating the mail for:", per, "\n", e$message)
+      return(NA)
+    }
+  )
+  
+  # Try to send the mail and save the data as vintage for the current subscriber
+  tryCatch(
+    {
+      # Send the mail
+      #source("./email_sender.R")
+      source("./vintage_saver.R")
+    },
+    error = function(e) {
+      cat("Error sending the mail or saving as vintage for:", per, "\n", e$message)
+      return(NA)
+    }
+  )
+      
+      
 }
 
