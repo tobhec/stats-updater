6b81a78579031bf6c8be17ef94c1d09ec717fdd2
Tobias Hector <tobias.hector@hotmail.com>
2025-11-23 22:09:13 +0100

    Added deletion for T-180 vintages

--- vintage_saver.R
+++ vintage_saver.R
@@ -1,9 +1,16 @@
 setwd("C:/Users/Tobia/raspberry_pi/stats-updater")
 
+# Delete vintages for T-180
+if (dir.exists(paste0("./vintages/", format(Sys.Date() - 180, "%Y-%m-%d")))) {
+  unlink(paste0("./vintages/", format(Sys.Date() - 180)), recursive = TRUE, force = TRUE)
+}
+
+# Create a new folder for todays date
 if (!dir.exists(paste0("./vintages/", format(Sys.Date(), "%Y-%m-%d")))) {
   dir.create(paste0("./vintages/", format(Sys.Date(), "%Y-%m-%d")), recursive = TRUE)
 }
 
+# Save each table as a separate file
 index <- 0
 for(table in tables_list) {
   index <- index + 1
